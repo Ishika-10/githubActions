802c424ed5fa68832793e4a63bd0e89a5acc0267
Ishika-10 <140721205+Ishika-10@users.noreply.github.com>
2024-11-29 09:54:25 +0530

    Add project files.

---
+++ githubActions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace githubActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
